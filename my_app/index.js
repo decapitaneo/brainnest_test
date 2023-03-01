0409781df94874d4da69954e0c380473ada6a373
Denise Capitaneo <decapitaneo@gmail.com>
2023-03-01 12:43:27 -0300

    Calculator test

---
+++ my_app/index.js
@@ -0,0 +1,23 @@
+
+class Calculator{
+//se nao guarda estado(propriedade/caracteristica), nao precisa de construtor
+    static sum(a,b){
+        return a+b;
+    }
+    
+    static sub(a,b){
+        return a-b;
+    }
+    
+    static multiply(a,b){
+        return a*b;
+    }
+    
+    static division(a,b){
+        return a/b;
+    };
+    
+}
+
+
+module.exports = {Calculator};
